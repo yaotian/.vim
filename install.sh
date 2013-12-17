c8b03e82c46e499e61332dd93ba59fe07526b5f8
yaotian <yaotian@gmail.com>
2013-12-17 17:52:20 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,32 +1,52 @@
 #!/usr/bin/env bash
 
-if [ -d ~/.vim ]
-then
-    echo "\033[0;33mYou already have vim configured.\033[0m You'll need to remove ~/.vim if you want to install/reinstall"
-    exit
-fi
-    
-    echo "\033[0;34mCloning dot-vimrc...\033[0m"
-    hash git >/dev/null && /usr/bin/env git clone https://github.com/yaotian/.vim.git || {
-    echo "git not installed"
-    exit
+warn ()
+{
+    printf '\033[0;31m%s\033[0m\n' "$1" >&2
 }
 
-echo "\033[0;34mCloning Vundle(required by dot-vimrc)...\033[0m"
-hash git >/dev/null && /usr/bin/env git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle || {
-  echo "git not installed"
-    exit
+die ()
+{
+    warn "$1"
+    exit 1
 }
 
-echo "\033[0;34mLooking for an existing vim config...\033[0m"
-if [ -f ~/.vimrc ] || [ -h ~/.vimrc ]
-then
-      echo "\033[0;33mFound ~/.vimrc.\033[0m \033[0;32]Backing up to ~/.vimrc.pre\033[0m";
-        cp ~/.vimrc ~/.vimrc.pre;
-          rm ~/.vimrc;
-      fi
-      
-      echo "\033[0;34mlink ~/.vimrc to ~/.vim/vimrc\033[0m"
-      ln -s ~/.vim/vimrc ~/.vimrc
-      
-      echo "\nYour vim is now configured!\nPlease open vim and run \"\033[44;37m:BundleInstall\033[0m\" command to finish the vim plugins installation.\033[0m""]"]]"]]]]]]]]]]]]]"
+printf '\033[0;34m%s\033[0m\n' "Thanks for installing yaotian's vim!"
+printf '\033[0;34m%s\033[0m\n' '                      '
+
+# check requirements
+printf '\033[0;34m%s\033[0m\n' "Checking requirements for vim..."
+printf '\033[0;34m%s\033[0m\n' "Checking vim version..."
+vim --version | grep 7.3 || vim --version | grep 7.4 || die "Your vim's version is too low!\nPlease download higher version(7.3+) from http://www.vim.org/download.php"
+printf '\033[0;34m%s\033[0m\n' "Checking if git exists..."
+which git || die "No git installed!\nPlease install git from http://git-scm.com/downloads/"
+printf '\033[0;34m%s\033[0m\n' "Check if ctags exists..."
+which ctags || warn "No ctags installed!\nPlease install ctags form http://ctags.sourceforge.net/ after ivim intallation!"
+
+# back up existing vim stuff
+printf '\033[0;34m%s\033[0m\n' "Backing up current vim config..."
+for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do [ -e $i ] && mv -f $i $i.backup; done
+
+# install ivim
+printf '\033[0;34m%s\033[0m\n' "Cloning ivim..."
+rm -rf $HOME/ivim
+git clone git://github.com/yaotian/.vim.git $HOME/.vim
+ln -s $HOME/.vim/vimrc $HOME/.vim/.vimrc
+
+printf '\033[0;34m%s\033[0m\n' "Installing Vundle..."
+git clone git://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
+
+printf '\033[0;34m%s\033[0m\n' "Installing hybrid theme..."
+git clone git://github.com/w0ng/vim-hybrid.git $HOME/.vim/bundle/vim-hybrid
+
+printf '\033[0;34m%s\033[0m\n' "Installing font for powerline..."
+mkdir $HOME/.fonts
+wget -O $HOME/.fonts/PowerlineSymbols.otf https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf
+fc-cache -vf $HOME/.fonts
+mkdir $HOME/.fonts.conf.d
+wget -O $HOME/.fonts.conf.d/10-powerline-symbols.conf https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
+
+printf '\033[0;34m%s\033[0m\n' "Installing plugins using Vundle..."
+vim +'set nospell' +BundleInstall! +BundleClean! +qa! $HOME/ivim/tools/info.txt
+
+printf '\033[0;34m%s\033[0m\n' "ivim has been installed. Just enjoy vimming!"
